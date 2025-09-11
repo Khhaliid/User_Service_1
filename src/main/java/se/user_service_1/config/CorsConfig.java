7ebebc8dbe9380744d4ddf7da1268fc599f3fca9
Erik <erik.thorsell.95@gmail.com>
2025-09-11 12:06:26 +0200

    CorsConfig för frontend

---
+++ src/main/java/se/user_service_1/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package se.user_service_1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {  // Stor bokstav!
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "http://localhost:5174"
+                        )
+                        .allowedMethods("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
